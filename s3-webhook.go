a34decaa767764b0505692ff454c988442289f67
Романенко Денис <romanenko.denis@mirafox.ru>
2020-06-02 15:23:40 +0300

    update

--- s3-webhook.go
+++ s3-webhook.go
@@ -13,19 +13,18 @@ import (
 )
 
 type subscriptionrequest struct {
-	Timestamp        *string `json:"Timestamp"`
-	Type             *string `json:"Type"`
-	Message          *string `json:"Message"`
-	TopicArn         *string `json:"TopicArn"`
-	SignatureVersion *int    `json:"SignatureVersion"`
-	Token            *string `json:"Token"`
+	Timestamp        string `json:"Timestamp"`
+	Type             string `json:"Type"`
+	Message          string `json:"Message"`
+	TopicArn         string `json:"TopicArn"`
+	SignatureVersion int    `json:"SignatureVersion"`
+	Token            string `json:"Token"`
 }
 
 // Generate hmac_sha256
 func HmacSha256(message string, secret string) string {
 	h := hmac.New(sha256.New, []byte(secret))
 	h.Write([]byte(message))
-	log.Printf("%x\n", h.Sum(nil))
 	return hex.EncodeToString(h.Sum(nil))
 }
 
@@ -52,11 +51,10 @@ func webhook(w http.ResponseWriter, req *http.Request) {
 
 	// Log token and uri
 	fullURI := "http://" + req.Host + req.URL.Path
-	log.Printf("Got token: %s for URI: %s\n", *subscr.Token, fullURI)
+	log.Printf("Got token: %s for URI: %s\n", subscr.Token, fullURI)
 
 	// Construct sinature responce
-	//	signature := HmacSha256(fullURI, HmacSha256(*subscr.TopicArn, HmacSha256(*subscr.Timestamp, *subscr.Token)))
-	signature := HmacSha256("http://test.com", HmacSha256("mcs2883541269|bucketA|s3:ObjectCreated:Put", HmacSha256("2019-12-26T19:29:12+03:00", "RPE5UuG94rGgBH6kHXN9FUPugFxj1hs2aUQc99btJp3E49tA")))
+	signature := HmacSha256(fullURI, HmacSha256(subscr.TopicArn, HmacSha256(subscr.Timestamp, subscr.Token)))
 	log.Printf("Generate responce signature: %s \n", signature)
 
 	// Send responce
