0ac71d904b905c7fd3d21ca4e20e2cd87c2a4f0c
Романенко Денис <romanenko.denis@mirafox.ru>
2020-06-05 17:00:17 +0300

    update s3

--- s3-webhook.go
+++ s3-webhook.go
@@ -95,7 +95,7 @@ func Webhook(w http.ResponseWriter, req *http.Request) {
     
 	// log request
 	log.Printf("[%s] incoming HTTP request from %s\n", req.Method, req.RemoteAddr)
-
+	log.Printf("%s\n",string(body))
 	// check if we got subscription confirmation request
 	if strings.Contains(string(body),"\"Type\":\"SubscriptionConfirmation\"") {
 		SubscriptionConfirmation(w,req,string(body))	
